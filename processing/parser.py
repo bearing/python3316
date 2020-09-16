26eb4d3b06e69e64bf6f2d25692ac9375de8e713
Justin Ellin <justinrellin@gmail.com>
2020-09-16 09:07:27 -0700

    Deal with single event outlier and can save first raw only. Test it

--- processing/parser.py
+++ processing/parser.py
@@ -9,7 +9,7 @@ class parser(object):
     def __init__(self, boards):
         self.boards = boards
         self.event_data = [c.event_stats for b in self.boards for c in b._chan]
-        self.event_id = 0
+        self.event_id = 1  # Starts from 1
 
     ADCWORDSIZE = np.dtype(np.uint16).newbyteorder('<')  # Events are read as 16 bit words, small endian
     FPGAWORDSIZE = np.dtype(np.uint32).newbyteorder('<')  # Events are read as 32 bit words, smallendian
@@ -48,13 +48,11 @@ class parser(object):
         if buffer.size is 0 or int(event_length) is 0:
             return None, None
 
-        if buffer.size is int(event_length):
-            pass  # TODO/FIXME: This is an unique case where the memory is read after only 1 event. Maybe force to 2+?
-
         try:
             data = {}
 
             if first_event_buffer_mode:
+                # You can run this and swap on single events, but it will be slower
                 first_evt = buffer[:event_length]
                 rest = buffer[event_length:]
                 offset = 2 + bool(current_event['acc1_flag']) * 7 \
@@ -77,28 +75,30 @@ class parser(object):
                 # new_buffer = np.concatenate((first_evt[:offset], first_evt[end:], rest))
 
                 event_length -= current_event['raw_event_length'] // 2  # All except first
-                evts = new_buffer.size // event_length
-                event_arr = new_buffer.reshape([evts, event_length])
 
+            else:
+                new_buffer = buffer
+
+            evts = new_buffer.size // event_length
+            # event_arr = new_buffer.reshape([evts, event_length])  # rows are events
+            event_arr = new_buffer.reshape([evts, event_length]).T  # columns are events
+
+            if first_event_buffer_mode:
                 rid = np.zeros(evts)
                 rid[0] = self.event_id
                 data['rid'] = rid  # Clumsy, lot of zeroes but oh well
                 self.event_id += 1
-
             else:
-                evts = buffer.size // event_length
-                event_arr = buffer.reshape([evts, event_length])
-
                 data['rid'] = self.event_id + np.arange(evts)
                 self.event_id += evts
-            # array where each row is an event
 
             # data = {'rid': self.event_id + np.arange(evts)}
             # data['rid'] = self.event_id + np.arange(evts)
             # self.event_id += evts
 
             # Always get these
-            ch_fmt = event_arr[:, 0] & 0xFFFF
+            # ch_fmt = event_arr[:, 0] & 0xFFFF  # If events are as rows
+            ch_fmt = event_arr[0] & 0xFFFF
 
             fmt = ch_fmt & 0b1111
 
@@ -106,51 +106,68 @@ class parser(object):
                 pass  # TODO: ERROR. save to raw instead
             # data['format'] = fmt
 
-            # ch = ch_fmt >> 4
+            data['det'] = ch_fmt >> 4
             # data['channel'] = (ch & 0b1111)  # Hardwired
             # data['module'] = (ch >> 4)  # Writeable. Can be module number allowing for detector number
-            data['det'] = ch_fmt >> 4
 
-            data['timestamp'] = ((event_arr[:, 0].astype(np.uint64) & 0xFFFF0000) << 16) + event_arr[:, 1]
+            # data['timestamp'] = ((event_arr[:, 0].astype(np.uint64) & 0xFFFF0000) << 16) + event_arr[:, 1]
+            data['timestamp'] = ((event_arr[0].astype(np.uint64) & 0xFFFF0000) << 16) + event_arr[1]
 
             pos = 2  # pointer to column where you are reading
 
             if bool(current_event['acc1_flag']):
-                data['adc_max'] = event_arr[:, pos] & 0xFFFF
-                data['adc_argmax'] = event_arr[:, pos] >> 16
-                data['gate1'] = event_arr[:, (pos+1)] & 0xFFFFff  # 24 bits
+                # data['adc_max'] = event_arr[:, pos] & 0xFFFF
+                # data['adc_argmax'] = event_arr[:, pos] >> 16
+                # data['gate1'] = event_arr[:, (pos+1)] & 0xFFFFff  # 24 bits
+                data['adc_max'] = event_arr[pos] & 0xFFFF
+                data['adc_argmax'] = event_arr[pos] >> 16
+                data['gate1'] = event_arr[pos+1] & 0xFFFFff  # 24 bits
 
-                info = (event_arr[:, (pos+1)] >> (24 + 4))  # First 4 bits "reserved"
-                data['pileup'] = (info & 0b1) | (info & 0b10)
-                # data['repileup'] = info & 0b10  # This is redundant information
+                info = (event_arr[pos + 1] >> (24 + 4))  # First 4 bits "reserved"
+                # info = (event_arr[:, (pos+1)] >> (24 + 4))
+                data['pileup'] = info & 0b11  # This should really be triggers. 1 is normal.
                 # data['underflow'] = info & 0b100
                 # data['overflow'] = info & 0b1000
 
-                data['gate2'] = event_arr[:, pos + 2]
-                data['gate3'] = event_arr[:, pos + 3]
-                data['gate4'] = event_arr[:, pos + 4]
-                data['gate5'] = event_arr[:, pos + 5]
-                data['gate6'] = event_arr[:, pos + 6]
+                # data['gate2'] = event_arr[:, pos + 2]
+                # data['gate3'] = event_arr[:, pos + 3]
+                # data['gate4'] = event_arr[:, pos + 4]
+                # data['gate5'] = event_arr[:, pos + 5]
+                # data['gate6'] = event_arr[:, pos + 6]
+                data['gate2'] = event_arr[pos + 2]
+                data['gate3'] = event_arr[pos + 3]
+                data['gate4'] = event_arr[pos + 4]
+                data['gate5'] = event_arr[pos + 5]
+                data['gate6'] = event_arr[pos + 6]
                 pos += 7
 
             if bool(current_event['acc2_flag']):
-                data['gate7'] = event_arr[:, pos]
-                data['gate8'] = event_arr[:, pos+1]
+                # data['gate7'] = event_arr[:, pos]
+                # data['gate8'] = event_arr[:, pos+1]
+                data['gate7'] = event_arr[pos]
+                data['gate8'] = event_arr[pos + 1]
                 pos += 2
 
             if bool(current_event['maw_flag']):
-                data['maw_max'] = event_arr[:, pos]
-                data['maw_before_trig'] = event_arr[:, (pos + 1)]
-                data['maw_after_trig'] = event_arr[:, (pos + 2)]
+                # data['maw_max'] = event_arr[:, pos]
+                # data['maw_before_trig'] = event_arr[:, (pos + 1)]
+                # data['maw_after_trig'] = event_arr[:, (pos + 2)]
+                data['maw_max'] = event_arr[pos]
+                data['maw_before_trig'] = event_arr[pos + 1]
+                data['maw_after_trig'] = event_arr[pos + 2]
                 pos += 3
 
             if bool(current_event['maw_energy_flag']):
-                data['en_start'] = event_arr[:, pos]
-                data['en_max'] = event_arr[:, (pos + 1)]
+                # data['en_start'] = event_arr[:, pos]
+                # data['en_max'] = event_arr[:, (pos + 1)]
+                data['en_start'] = event_arr[pos]
+                data['en_max'] = event_arr[pos + 1]
                 pos += 2
 
-            raw_samples = event_arr[:, pos] & 0x3FFffff  # in 32-bit words
-            OxE_maw_status = event_arr[:, pos] >> 26
+            # raw_samples = event_arr[:, pos] & 0x3FFffff  # in 32-bit words
+            # OxE_maw_status = event_arr[:, pos] >> 26
+            raw_samples = event_arr[pos] & 0x3FFffff  # in 32-bit words
+            OxE_maw_status = event_arr[pos] >> 26
             OxE, maw_test, status_flag = OxE_maw_status >> 2, (OxE_maw_status >> 1 & 0b1), (OxE_maw_status & 0b1)
             # data['maw_select'] = maw_test  # Trigger MAW (0) or Energy MAW (1)
             # data['status'] = status_flag   # Pileup or Repileup
@@ -169,17 +186,25 @@ class parser(object):
             raw_samples = 2 * raw_samples[0]  # in 16 bit words
 
             if raw_samples and not first_event_buffer_mode:
-                raw_words = event_arr[:, pos:(pos + raw_samples)]
+                # raw_words = event_arr[:, pos:(pos + raw_samples)]
+                raw_words = event_arr[pos:(pos + raw_samples)]
+
                 # raw_data = np.zeros((2 * raw_words.size,), dtype=self.ADCWORDSIZE)
-                raw_data = np.zeros([evts, raw_samples], dtype=np.uint16)
-                raw_data[:, 0::2] = raw_words & 0xFFFF
-                raw_data[:, 1::2] = raw_words >> 16
-                data['raw_data'] = raw_data
+                # raw_data = np.zeros([evts, raw_samples], dtype=np.uint16)
+                # raw_data[:, 0::2] = raw_words & 0xFFFF
+                # raw_data[:, 1::2] = raw_words >> 16
+                raw_data = np.zeros([raw_samples, evts], dtype=np.uint16)
+                raw_data[0::2] = raw_words & 0xFFFF
+                raw_data[1::2] = raw_words >> 16
+
+                # data['raw_data'] = raw_data
+                data['raw_data'] = raw_data.T  # Transpose this into rows
                 pos += raw_samples
 
             maw_samples = current_event['maw_event_length']
             if maw_samples:
-                data['maw_data'] = event_arr[:, pos:(pos + maw_samples)]
+                # data['maw_data'] = event_arr[:, pos:(pos + maw_samples)]
+                data['maw_data'] = event_arr[pos:(pos + maw_samples)].T
 
             return data, evts
 

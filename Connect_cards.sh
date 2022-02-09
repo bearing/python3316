b81e575f0143b6174b344c6496c1b21027e19b5b
cllamb0 <cllamb777@gmail.com>
2022-02-09 15:15:18 -0800

    Added 3316 card connections for MKS PGI

--- Connect_cards.sh
+++ Connect_cards.sh
@@ -1,9 +1,14 @@
 #Array of card names, purely for aesthetical purposes can be named whatever
-declare -a CardNames=("Card 1" "Card 2" "Card 10" "Card 12" "Card 6" "Card 7" "Card 4" "Card 5")
+declare -a CardNames=("Card 1" "Card 2" "Card 10" "Card 12" "Card 6" "Card 7" "Card 4" "Card 5") # CAMIS
+#declare -a CardNames=("Card 3" "Card 9" "Card 11" "Card 13") # MKS PGI
+
 #Card ID numbers, found on the back of the 3316 cards. Last 3 digits (or 2 if third to last is 0)
-declare -a CardIDs=("66" "67" "54" "68" "111" "215" "69" "216")
+declare -a CardIDs=("66" "67" "54" "68" "111" "215" "69" "216") # CAMIS
+#declare -a CardIDS=("71" "109" "70" "397") # MKS PGI
+
 #IP numbers to assign the cards when running, can range from 2-16
-declare -a CardIPs=("2" "3" "4" "5" "6" "7" "8" "9")
+declare -a CardIPs=("2" "3" "4" "5" "6" "7" "8" "9") # CAMIS
+#declare -a CardIPS=("2" "3" "4" "5") # MKS PGI
 
 a=enp5s0
 echo Performing intial setup config$'\n'
